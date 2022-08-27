0b7c48a4cd1d038e115fd3febe35401ea484b159
makutoro_desktop <tw21wryy@gmail.com>
2022-08-27 05:47:53 +0000

    header固定

---
+++ app/javascript/packs/header.js
@@ -0,0 +1,15 @@
+// 指定したIDの要素を返却(id名,css要素名)
+// 例: (#body,height) => 30px
+function fixHeaderPadding(){ 
+  let nav_height = $('#header-nav').css('height');
+  $('#header').css('padding-bottom', nav_height);
+}
+
+// ページが読み込まれるたびに呼び出し
+$(window).on('turbolinks:load', function() {
+  fixHeaderPadding() //nav-barの高さに合わせてheaderのpaddingを自動調整
+  console.log(Date.now() + " #header padding-bottom =>" + $('#header').css('padding-bottom'));
+});
+
+$(window).scroll(function () {
+});
